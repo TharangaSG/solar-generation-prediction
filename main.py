5f5cc487eff6f7150372c56f0c38594ca2250d89
TharangaSG <tharangasg20@gmail.com>
2025-03-28 15:30:49 +0530

    updated main.py

--- main.py
+++ main.py
@@ -5,41 +5,41 @@ from src.data.make_dataframe import MakeDataframe
 from src.hopsworks_integration.feature_store import HopsworksFeatureStore
 from src.features.remove_outliers import OutlierDetection
 
-# STAGE_NAME = "Make dataset"
-# try:
-#     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
-#     make_dataset = MakeDataset()
-#     make_dataset.make_generation_csv()
-#     make_dataset.make_weather_csv()
-#     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
-# except Exception as e:
-#         logger.exception(e)
-#         raise e
+STAGE_NAME = "Make dataset"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+    make_dataset = MakeDataset()
+    make_dataset.make_generation_csv()
+    make_dataset.make_weather_csv()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
 
 
-# STAGE_NAME = "Make dataframe"
-# try:
-#     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
-#     make_dataframe = MakeDataframe()
-#     make_dataframe.make_generation_dataframe()
-#     make_dataframe.make_weather_dataframe()
-#     make_dataframe.make_combined_dataframe()
-#     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
-# except Exception as e:
-#         logger.exception(e)
-#         raise e
+STAGE_NAME = "Make dataframe"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+    make_dataframe = MakeDataframe()
+    make_dataframe.make_generation_dataframe()
+    make_dataframe.make_weather_dataframe()
+    make_dataframe.make_combined_dataframe()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
 
 
-# STAGE_NAME = "Make feature group for raw data"
-# try:
-#     logger.info(f">>>>>> Stage {STAGE_NAME} started <<<<<<")
-#     hopsworks_feature_store = HopsworksFeatureStore()
-#     hopsworks_feature_store.upload_fg()
+STAGE_NAME = "Make feature group for raw data"
+try:
+    logger.info(f">>>>>> Stage {STAGE_NAME} started <<<<<<")
+    hopsworks_feature_store = HopsworksFeatureStore()
+    hopsworks_feature_store.upload_fg()
     
-#     logger.info(f">>>>>> Stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
-# except Exception as e:
-#     logger.exception(e)
-#     raise e
+    logger.info(f">>>>>> Stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
 
 STAGE_NAME = "Outlier detection"
 try:
